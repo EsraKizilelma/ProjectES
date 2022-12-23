19f9721838c6df52fa81ed300a4c14fc2f581650
Sedanur Koçal <94858634+SedanurKocal@users.noreply.github.com>
2022-12-23 16:12:27 +0300

    loginde yenilikler

--- ProjectES/Data/Migrations/20221223122725_name.Designer.cs
+++ ProjectES/Data/Migrations/20221223122725_name.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectES.Data;
 
@@ -11,9 +12,10 @@ using ProjectES.Data;
 namespace ProjectES.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221223122725_name")]
+    partial class name
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjectES/Data/Migrations/20221223122725_name.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectES.Data.Migrations
+{
+    public partial class name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ad",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Soyad",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ad",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Soyad",
+                table: "AspNetUsers");
+        }
+    }
+}
