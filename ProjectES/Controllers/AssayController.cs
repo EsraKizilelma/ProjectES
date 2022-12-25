63563369dd38be384ee7591b0dd32fe42a0fa7f4
Sedanur Koçal <94858634+SedanurKocal@users.noreply.github.com>
2022-12-25 22:48:12 +0300

    assay controller eklendi

--- ProjectES/Controllers/AssayController.cs
+++ ProjectES/Controllers/AssayController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProjectES.Data;
 using ProjectES.Models;
+using System.Drawing;
 
 namespace ProjectES.Controllers
 {
@@ -23,7 +25,146 @@ namespace ProjectES.Controllers
 
             return View(assay);
         }
+        //public IActionResult Create()
+        //{
 
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult Create([Bind("SubjectName,SubjectType")] Subject a)
+        //{
+        //    _context.Add(a);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        // GET: Kitap/Create
+        public IActionResult Create()
+        {
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "SubjectName");
+            return View();
+        }
+
+        // POST: Kitap/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AssayId,AssayTitle,SubjectId")] Assay assay)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(assay);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "SubjectId", "YazarAd", assay.SubjectId);
+            return View(assay);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.SubjectId == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            return View(subject);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SubjectId,SubjectName,SubjectType")] Subject subject)
+        {
+            if (id != subject.SubjectId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubjectExists(subject.SubjectId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subject);
+        }
+        //**************
+
+
+        // GET: Subject1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.SubjectId == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: Subject1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Subjects == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Subjects'  is null.");
+            }
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject != null)
+            {
+                _context.Subjects.Remove(subject);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubjectExists(int id)
+        {
+            return _context.Subjects.Any(e => e.SubjectId == id);
+        }
         //public IActionResult Index()
         //{
         //    List<Assay> a = _context.Assays.Include(aa => aa.Subj).ToList();
--- ProjectES/Controllers/Subject2Controller.cs
+++ ProjectES/Controllers/Subject2Controller.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjectES.Data;
 using ProjectES.Models;
 
@@ -27,6 +28,111 @@ namespace ProjectES.Controllers
             _context.Add(a);
             _context.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.SubjectId == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+            return View(subject);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SubjectId,SubjectName,SubjectType")] Subject subject)
+        {
+            if (id != subject.SubjectId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubjectExists(subject.SubjectId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subject);
+        }
+        //**************
+
+
+        // GET: Subject1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Subjects == null)
+            {
+                return NotFound();
+            }
+
+            var subject = await _context.Subjects
+                .FirstOrDefaultAsync(m => m.SubjectId == id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return View(subject);
+        }
+
+        // POST: Subject1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Subjects == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Subjects'  is null.");
+            }
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject != null)
+            {
+                _context.Subjects.Remove(subject);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubjectExists(int id)
+        {
+            return _context.Subjects.Any(e => e.SubjectId == id);
         }
     }
 }
