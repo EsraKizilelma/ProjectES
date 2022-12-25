246bc2c8c532df370354a87dfcbd89ced05c6142
Esra KIZILELMA <kizilelmaesra@gmail.com>
2022-12-25 19:23:51 +0300

    linq işlemi

---
+++ ProjectES/Controllers/AssayController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectES.Data;
+using ProjectES.Models;
+
+namespace ProjectES.Controllers
+{
+    public class AssayController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AssayController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Assay> a = _context.Assays.Include(aa => aa.Subj).ToList();
+
+            var assay = (from aa in _context.Assays where aa.Subj != null select aa).ToList();
+
+
+
+            return View(assay);
+        }
+
+        //public IActionResult Index()
+        //{
+        //    List<Assay> a = _context.Assays.Include(aa => aa.Subj).ToList();
+
+        //    return View(a);
+        //}
+
+    }
+}
--- ProjectES/Models/Subject.cs
+++ ProjectES/Models/Subject.cs
@@ -1,10 +1,21 @@
-﻿namespace ProjectES.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectES.Models
 {
     public class Subject
     {
         public int SubjectId { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Subject Name")]
         public string SubjectName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(50,ErrorMessage = "Over the MaxLength")]
+        [Display(Name = "Subject Type")]
         public string SubjectType { get; set; }
+
         public ICollection<Assay> Assays { get; set; }
     }
 }
