30fbb96e1bf3f6a77db9f273a8c3ecc4a27ac2da
Sedanur Koçal <94858634+SedanurKocal@users.noreply.github.com>
2022-12-29 17:58:36 +0300

    api eklendi

---
+++ ProjectES/Controllers/AssayApiController.cs
@@ -0,0 +1,48 @@
+﻿using ProjectES.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectES.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectES.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssayApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public AssayApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<ActionResult<List<Assay>>> Get()
+        {
+            var y = await _context.Assays.ToListAsync();
+            if (y is null)
+            {
+                return NoContent();
+            }
+            return y;
+
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Assay>> Get(int id)
+        {
+            var y = await _context.Assays.FirstOrDefaultAsync(x=>x.AssayId==id);
+            if (y is null)
+            {
+                return NoContent();
+            }
+            return y;
+        }
+    }
+}
---
+++ ProjectES/Controllers/CallAssayApiController.cs
@@ -0,0 +1,31 @@
+﻿using ProjectES.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NuGet.ContentModel;
+
+namespace ProjectES.Controllers
+{
+    public class CallAssayApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Assay> Assays = new List<Assay>();
+            var hhtc = new HttpClient();
+            var response = await hhtc.GetAsync("https://localhost:7169/api/AssayApi");
+            string resString = await response.Content.ReadAsStringAsync();
+			Assays = JsonConvert.DeserializeObject<List<Assay>>(resString);
+            return View(Assays);
+        }
+        //List<Assay> a = _context.Assays.Include(aa => aa.Subj).ToList();
+
+        //var assay = (from aa in _context.Assays where aa.Subj != null select aa).ToList();
+        //    return View(assay);
+    }
+}
